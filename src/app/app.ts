d5133501914a93be665ce45179d37706e7076ee3
Mohamed Zitouni <mohamed.zitouni@external.grtgaz.com>
2025-12-23 11:40:38 +0100

    deploy to github pages

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('demo-angular');
+  protected readonly title = signal('My ZBlog');
 }
