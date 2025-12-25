f7babf8870bb63b6ea10ce99ff19a5dde75f3d94
Mohamed Zitouni <mohamed.zitouni@external.grtgaz.com>
2025-12-25 09:17:48 +0100

    clean code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,6 @@
-import {Component, effect, inject, Renderer2, signal} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { LayoutComponent } from './core/layout/components/layout.component';
+import {Component, effect, inject, Renderer2} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {LayoutComponent} from './core/layout/components/layout.component';
 import {LayoutService} from './core/layout/services/layout.service';
 
 @Component({
@@ -13,6 +13,7 @@ export class App {
   private readonly renderer = inject(Renderer2);
   private readonly DARK_THEME = 'dark-theme';
   private layoutService = inject(LayoutService);
+
   constructor() {
     this.initEffetcs();
   }
@@ -20,7 +21,7 @@ export class App {
   private initEffetcs() {
     effect(() => {
       const isDarkMode = this.layoutService.isDarkMode();
-      if(isDarkMode()) {
+      if (isDarkMode()) {
         this.renderer.addClass(document.body, this.DARK_THEME)
       } else {
         this.renderer.removeClass(document.body, this.DARK_THEME)
