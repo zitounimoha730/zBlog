d3cd0fdbfe8209b5dc88f46e14274cc91eb83a23
Mohamed Zitouni <mohamed.zitouni@external.grtgaz.com>
2025-12-31 11:33:06 +0100

    Better nav bar

--- src/app/core/layout/components/menu/courses-menu/courses-menu.component.ts
+++ src/app/core/layout/components/menu/courses-menu/courses-menu.component.ts
@@ -1,26 +1,46 @@
 import {ChangeDetectionStrategy, Component, inject, ViewEncapsulation,} from '@angular/core';
-import {MatButton} from '@angular/material/button';
-import {MatMenu, MatMenuItem, MatMenuTrigger} from '@angular/material/menu';
 import {CoursesService} from '../../../../../courses/services/courses.service';
 import {CourseModel} from '../../../../../courses/models/course.model';
 import {Router} from '@angular/router';
 
+export interface MenuItem {
+  label: string;
+  children?: MenuItem[];
+  expanded?: boolean;
+}
+
 @Component({
   selector: 'zblog-courses-menu',
-  imports: [
-    MatButton,
-    MatMenu,
-    MatMenuItem,
-    MatMenuTrigger,
-  ],
+  imports: [],
   templateUrl: './courses-menu.component.html',
   styleUrl: './courses-menu.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
 })
 export class CoursesMenuComponent {
+  menu: MenuItem[] = [
+    {
+      label: 'Angular',
+      expanded: true,
+      children: [
+        {label: 'Angular 14'},
+        {label: 'Angular 20'}
+      ]
+    },
+    {
+      label: 'Java',
+      children: [
+        {label: 'Java 8'},
+        {label: 'Java 25'}
+      ]
+    }
+  ];
   protected coursesService = inject(CoursesService);
   private router = inject(Router);
+
+  toggle(item: MenuItem) {
+    item.expanded = !item.expanded;
+  }
 
   protected goToCourse(course: CourseModel) {
     this.router.navigate([course.key]).then();
