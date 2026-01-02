1d62daf19a44eccefa65f371a1bfdfcd3a2a2f7f
Mohamed Zitouni <mohamed.zitouni@external.grtgaz.com>
2026-01-02 12:24:28 +0100

    fix app menu and center align

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,13 @@ import {Component, effect, inject, Renderer2} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {LayoutComponent} from './core/layout/components/layout.component';
 import {LayoutService} from './core/layout/services/layout.service';
+import {Observable} from 'rxjs';
+import {ResponsiveService} from './core/layout/services/responsive.service';
+import {AsyncPipe, NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LayoutComponent],
+  imports: [RouterOutlet, LayoutComponent, NgClass, AsyncPipe],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
@@ -13,9 +16,14 @@ export class App {
   private readonly renderer = inject(Renderer2);
   private readonly DARK_THEME = 'dark-theme';
   private layoutService = inject(LayoutService);
+  private responsiveService = inject(ResponsiveService);
 
   constructor() {
     this.initEffetcs();
+  }
+
+  protected isMobile(): Observable<boolean> {
+    return this.responsiveService.isMobile();
   }
 
   private initEffetcs() {
