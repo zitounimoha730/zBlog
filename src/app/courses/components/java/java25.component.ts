9ddb60657edc25fbe8304c050afb2038d6a3d454
Mohamed Zitouni <zitounimoha730@gmail.com>
2026-02-25 15:44:53 +0100

    last version

--- src/app/courses/components/java/java25.component.ts
+++ src/app/courses/components/java/java25.component.ts
@@ -7,7 +7,12 @@ import hljs from 'highlight.js';
   imports: [],
   template: `
     <h3>Java 25</h3>
-    <p>.....</p>
+    <h4>Module Import Declarations (JEP 511 - Preview)</h4>
+    <pre><code class="java">{{ code1 }}</code></pre>
+    <h4>Coalesced Module Imports</h4>
+    <pre><code class="java">{{ code2 }}</code></pre>
+    <h4>Compact Source Files (JEP 512) and Instance Main Methods</h4>
+    <pre><code class="java">{{ code3 }}</code></pre>
   `,
 })
 export class Java25Component extends AbstractCourseComponent implements AfterViewInit {
@@ -17,4 +22,44 @@ export class Java25Component extends AbstractCourseComponent implements AfterVie
     });
   }
 
+  protected code1 = `
+import module java.base;
+import module java.sql;
+
+import java.sql.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date d = Date.valueOf("2025-06-15");
+        System.out.println("Resolved Date: " + d);
+    }
+}
+  `;
+
+  protected code2 = `
+// These imports could be coalesced:
+import javax.xml.*; 
+import javax.xml.parsers.*; 
+import javax.xml.stream.*;
+
+// Instead, just use:
+import module java.xml;
+`;
+
+protected code3 = `
+void main() {
+    System.out.println("Hello from Java 25!");
+}
+
+void main() {
+    IO.println("Hello, World!");
+}
+
+// The IO class provides simplified I/O methods:
+public static void print(Object obj);
+public static void println(Object obj);
+public static void println();
+public static String readln(String prompt);
+public static String readln();
+ `;
 }
